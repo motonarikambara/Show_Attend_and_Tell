Fix metrics calculation

data_loader.py
@@ -18,73 +18,131 @@ import caption_generator
 
 class DataLoader():
     def __init__(self):
-        annotation_zip = tf.keras.utils.get_file('captions.zip',
-                                                cache_subdir=os.path.abspath('.'),
-                                                origin = 'http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
-                                                extract = True)
-        annotation_file = os.path.dirname(annotation_zip)+'/annotations/captions_train2014.json'
+        # annotation_zip = tf.keras.utils.get_file('captions.zip',
+        #                                         cache_subdir=os.path.abspath('.'),
+        #                                         origin = 'http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
+        #                                         extract = True)
+        # annotation_file = os.path.dirname(annotation_zip)+'/annotations/captions_train2014.json'
 
-        name_of_zip = 'train2014.zip'
-        if not os.path.exists(os.path.abspath('.') + '/' + name_of_zip):
-            image_zip = tf.keras.utils.get_file(name_of_zip,
-                                                cache_subdir=os.path.abspath('.'),
-                                                origin = 'http://images.cocodataset.org/zips/train2014.zip',
-                                                extract = True)
-            PATH = os.path.dirname(image_zip)+'/train2014/'
-        else:
-            PATH = os.path.abspath('.')+'/train2014/'
-        # annotation_dir = 'aben20latest/labels'
-        # train_annot = os.path.dirname(annotation_dir)+'/sentences_train_wrs.csv'
-        # val_annot = os.path.dirname(annotation_dir)+'/sentences_val_wrs.csv'
-        # test_annot = os.path.dirname(annotation_dir)+'/sentences_test_wrs.csv'
+        # name_of_zip = 'train2014.zip'
+        # if not os.path.exists(os.path.abspath('.') + '/' + name_of_zip):
+        #     image_zip = tf.keras.utils.get_file(name_of_zip,
+        #                                         cache_subdir=os.path.abspath('.'),
+        #                                         origin = 'http://images.cocodataset.org/zips/train2014.zip',
+        #                                         extract = True)
+        #     PATH = os.path.dirname(image_zip)+'/train2014/'
+        # else:
+        #     PATH = os.path.abspath('.')+'/train2014/'
+        annotation_dir = os.path.abspath('.') + '/wsr20/labels/'
+        train_annot = os.path.dirname(annotation_dir) + '/sentences_train_wrs.csv'
+        train_imgs = os.path.dirname(annotation_dir) + '/image_id_train.csv'
+        val_annot = os.path.dirname(annotation_dir) + '/sentences_val_wrs.csv'
+        val_imgs = os.path.dirname(annotation_dir) + '/image_id_val.csv'
+        test_annot = os.path.dirname(annotation_dir) + '/sentences_test_wrs.csv'
+        test_imgs = os.path.dirname(annotation_dir) + '/image_id_test.csv'
+        test_target = os.path.dirname(annotation_dir) + '/target_id_test.csv'
+        val_target = os.path.dirname(annotation_dir) + '/target_id_val.csv'
 
-        # # json ファイルの読み込み
-        # with open(train_annot, 'r') as f:
-        #     trainannotations = csv.readers(f)
-        # with open(val_annot, 'r') as f:
-        #     valannotations = csv.readers(f)
-        # with open(test_annot, 'r') as f:
-        #     testannotations = csv.readers(f)
+
+        trainannotations = []
+        trainimgs = []
+        valannotations = []
+        valimgs = []
+        testannotations = []
+        testimgs = []
+        targetid = []
+        valtargetid= []
+        # csvファイルの読み込み
+        with open(train_annot, 'r') as f:
+            tmp = csv.reader(f)
+            for i in f:
+                trainannotations.append(i)
+        with open(train_imgs, 'r') as f:
+            tmp = csv.reader(f)
+            for i in f:
+                trainimgs.append(i)
+        with open(val_annot, 'r') as f:
+            tmp = csv.reader(f)
+            for i in f:
+                valannotations.append(i)
+        with open(val_imgs, 'r') as f:
+            tmp = csv.reader(f)
+            for i in f:
+                valimgs.append(i)
+        with open(test_annot, 'r') as f:
+            tmp = csv.reader(f)
+            for i in f:
+                testannotations.append(i)
+        with open(test_imgs, 'r') as f:
+            tmp = csv.reader(f)
+            for i in f:
+                testimgs.append(i)
+        with open(test_target, 'r') as f:
+            tmp = csv.reader(f)
+            for i in f:
+                targetid.append(i)
+        with open(val_target, 'r') as f:
+            tmp = csv.reader(f)
+            for i in f:
+                valtargetid.append(i)
+        #############################################################
+        tmptestannotations = []
+        tmptest_annot = os.path.abspath('.') + '/generated_sentence_test000.txt'
+        with open(tmptest_annot, 'r') as f:
+            for i in f:
+                tmptestannotations.append(i)
+        ###############################################################
+
+        PATH = os.path.abspath('.') + '/wsr20/single_view_img_data/'
         # json ファイルの読み込み
-        with open(annotation_file, 'r') as f:
-            annotations = json.load(f)
+        # with open(annotation_file, 'r') as f:
+        #     annotations = json.load(f)
 
+        # image id & caption
+        tr_caption = []
+        tr_imgs = []
+        va_caption = []
+        va_imgs = []
+        tes_caption = []
+        tes_imgs = []
+        tmptes_caption = []
 
+        for i in range(len(trainannotations)):
+            caption = '<start> ' + trainannotations[i] + ' <end>'
+            tr_caption.append(caption)
+            image_path = PATH + '%04d.jpg' % (int(trainimgs[i]))
+            tr_imgs.append(image_path)
 
-        # ベクトルにキャプションと画像の名前を格納
-        all_captions = []
-        all_img_name_vector = [] 
-        count = 0
-        for annot in annotations['annotations']:
-            caption = '<start> ' + annot['caption'] + ' <end>'
-            image_id = annot['image_id']
-            full_coco_image_path = PATH + 'COCO_train2014_' + '%012d.jpg' % (image_id)
-            if count <= 9999:
-                all_img_name_vector.append(full_coco_image_path)
-                all_captions.append(caption)
-                count += 1 
-            else:
-                break
+        for i in range(len(valannotations)):
+            caption = '<start> ' + valannotations[i] + ' <end>'
+            va_caption.append(caption)
+            image_path = PATH + '%04d.jpg' % (int(valimgs[i]))
+            va_imgs.append(image_path)
+        
+        for i in range(len(testannotations)):
+            # caption = '<start> ' + testannotations[i] + ' <end>'
+            caption = testannotations[i]
+            tes_caption.append(caption)
+            image_path = PATH + '%04d.jpg' % (int(testimgs[i]))
+            tes_imgs.append(image_path)
+        
+        for i in range(len(tmptestannotations)):
+            caption = tmptestannotations[i]
+            tmptes_caption.append(caption)
 
-        # captions と image_names を一緒にシャッフル
-        # random_state を設定
-        self.train_captions, self.img_name_vector = shuffle(all_captions,
-                                                all_img_name_vector,
+        self.cheat = tmptes_caption
+
+        self.train_captions, self.train_images = shuffle(tr_caption,
+                                                tr_imgs,
                                                 random_state=1)
-        self.train_cap = []
-        self.train_img = []
-        self.test_cap = []
-        self.test_img = []
-        for i in range(len(all_captions)):
-            if i < 8000:
-                self.train_cap.append(all_captions[i])
-                self.train_img.append(all_img_name_vector[i])
-            else : 
-                self.test_cap.append(all_captions[i])
-                self.test_img.append(all_img_name_vector[i])
-
-
-
+        self.val_captions, self.val_images = shuffle(va_caption,
+                                                va_imgs,
+                                                random_state=1)
+        self.test_captions = tes_caption
+        self.test_images = tes_imgs
+        self.test_target = targetid
+        self.val_target = valtargetid
+                                 
     def load_image(self, image_path):
         img = tf.io.read_file(image_path)
         img = tf.image.decode_jpeg(img, channels=3)
@@ -112,7 +170,7 @@ class DataLoader():
         self.image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
 
         # 重複のない画像を取得
-        encode_train = sorted(set(self.train_img))
+        encode_train = sorted(set(self.train_images))
 
         # batch_size はシステム構成に合わせて自由に変更可能
         image_dataset = tf.data.Dataset.from_tensor_slices(encode_train)
@@ -134,14 +192,14 @@ class DataLoader():
     def tokenize(self):
         self.tokenizer = tf.keras.preprocessing.text.Tokenizer(oov_token="<unk>",
                                                         filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
-        self.tokenizer.fit_on_texts(self.train_cap)
-        train_seqs = self.tokenizer.texts_to_sequences(self.train_cap)
+        self.tokenizer.fit_on_texts(self.train_captions)
+        train_seqs = self.tokenizer.texts_to_sequences(self.train_captions)
 
         self.tokenizer.word_index['<pad>'] = 0
         self.tokenizer.index_word[0] = '<pad>'
 
         # トークン化したベクトルを生成
-        train_seqs = self.tokenizer.texts_to_sequences(self.train_cap)
+        train_seqs = self.tokenizer.texts_to_sequences(self.train_captions)
 
         # キャプションの最大長に各ベクトルをパディング
         # max_length を指定しない場合、pad_sequences は自動的に計算

main.py
@@ -13,6 +13,7 @@ from glob import glob
 from PIL import Image
 import pickle
 from tqdm import tqdm
+import datetime
 
 import reg_bleu_score as bleu_score
 from pycocoevalcap.bleu.bleu import Bleu
@@ -28,13 +29,17 @@ class ShowAttendandTell():
         self.tokenizer = self.captiongenerator.tokenizer
 
     def train(self):
+        self.ans = []
+        self.res = 0
+        self.score = {'BLEU4':0, 'ROUGE':0, 'METEOR':0, 'CiDEr':0}
         print("Start trining")
         ckpt_manager, self.dataset = self.captiongenerator.model()
         start_epoch = 0
         if ckpt_manager.latest_checkpoint:
             start_epoch = int(ckpt_manager.latest_checkpoint.split('-')[-1])
 
-        EPOCHS = 3
+        start_epoch = 0
+        EPOCHS = 1
         for epoch in range(start_epoch, EPOCHS):
             start = time.time()
             total_loss = 0
@@ -47,62 +52,94 @@ class ShowAttendandTell():
                     print ('Epoch {} Batch {} Loss {:.4f}'.format(
                     epoch + 1, batch, batch_loss.numpy() / int(target.shape[1])))
 
-            if epoch % 5 == 0:
-                ckpt_manager.save()
+            # if epoch % 5 == 0:
+            #     ckpt_manager.save()
 
             print ('Epoch {} Loss {:.6f}'.format(epoch + 1,
                                                 total_loss/self.captiongenerator.num_steps))
             print ('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
-        print("End training")
+            print("Start validation")
+            self.test(self.captiongenerator.img_name_val, self.captiongenerator.cap_val)
+            print("End validation")
+            if self.res < self.me:
+                self.res = self.me
+                ckpt_manager.save()
+                print("Start test")
+                self.test(self.captiongenerator.img_name_test, self.captiongenerator.cap_test, test=True)
+                print("End test")
 
-    def test(self):
+        print("End training")
+        now = datetime.datetime.now()
+        path_cap = 'result/{0:%Y%m%d}_cap.txt'.format(now)
+        path_img = 'result/{0:%Y%m%d}_img.txt'.format(now)
+        with open(path_cap, mode='w') as f:
+            f.write('\n'.join(self.ans))
+        with open(path_img, mode='w') as f:
+            f.write('\n'.join(self.captiongenerator.img_name_test))
+        print("BLEU4: ", self.score['BLEU4'])
+        print("ROUGE: ", self.score['ROUGE'])
+        print("METEOR: ", self.score['METEOR'])
+        print("CiDEr: ", self.score['CiDEr'])
+
+    def test(self, img, caption, test=False):
         print("Start test")
         # 検証用セットのキャプション
-        results = list()
-        # rid = np.random.randint(0, len(self.captiongenerator.img_name_val))
-        # image = self.captiongenerator.img_name_val[rid]
-        # real_caption = ' '.join([self.tokenizer.index_word[i] for i in self.captiongenerator.cap_val[rid] if i not in [0]])
-        # result = self.captiongenerator.evaluate(image)
-        for i in tqdm(range(len(self.captiongenerator.img_name_val))):
-            image = self.captiongenerator.img_name_val[i]
-            result = self.captiongenerator.evaluate(image)
+        # results = list()
+        # for i in tqdm(range(len(img))):
+        #     image = img[i]
+        #     result = self.captiongenerator.evaluate(image)
 
-            g_sent = ' '.join(result)
-            res = g_sent.split('.')[0]
-            final_sent = res.split('?')[0].rstrip()
-            results.append(final_sent)
-
-        ans = self.captiongenerator.cap_val       
+        #     g_sent = ' '.join(result)
+        #     res = g_sent.split('.')[0]
+        #     final_sent = res.split('?')[0].rstrip()
+        #     results.append(final_sent)
+        ans = caption       
         for i in range(len(ans)):
             ans[i] = ans[i].rstrip()
         
+        ########################################################
+        # metric test
+        # 実行時には消すこと
+        results = self.captiongenerator.cheat
+        for i in range(len(results)):
+            results[i] = results[i].rstrip()
+        # for i in range(len(ans)):
+        #     ans[i] = ans[i].rstrip()
+        #########################################################
+
+
         reference_dict = dict()
         generated_dict = dict()
-        for iid, tid, ref, gen in zip(self.captiongenerator.img_name_val, self.captiongenerator.img_name_val, ans, results):
-                reference_dict.setdefault(str(iid).zfill(
-                    4) + "_" + str(tid).zfill(4), []).append(self.metric_preprocess([ref])[0])
-                generated_dict[str(iid).zfill(
-                    4) + "_" + str(tid).zfill(4)] = self.metric_preprocess([gen])
+        if not test:
+            target_id = self.captiongenerator.val_target
+            for iid, tid, ref, gen in zip(img, target_id, ans, results):
+                    reference_dict.setdefault(str(iid).zfill(
+                        4) + "_" + str(tid).zfill(4), []).append(self.metric_preprocess([ref])[0])
+                    generated_dict[str(iid).zfill(
+                        4) + "_" + str(tid).zfill(4)] = self.metric_preprocess([gen])
+        else:
+            target_id = self.captiongenerator.test_target
+            for iid, tid, ref, gen in zip(img, target_id, ans, results):
+                    reference_dict.setdefault(str(iid).zfill(
+                        4) + "_" + str(tid).zfill(4), []).append(self.metric_preprocess([ref])[0])
+                    generated_dict[str(iid).zfill(
+                        4) + "_" + str(tid).zfill(4)] = self.metric_preprocess([gen])
         # Make dict to calcurate metrics
+        # print(reference_dict)
 
         total_score, sc_bl, sc_rg, sc_me, sc_cid = self.calculate_metric_scores(reference_dict, generated_dict)
         print("BLEU4: %.3f" % sc_bl[3])
         print("ROUGE: %.3f" % sc_rg)
         print("METEOR: %.3f" % sc_me)
-        print("Cider: %.3f" % sc_cid)
-        # print ('Real Caption:', real_caption)
-        # print ('Prediction Caption:', ' '.join(result))
+        print("CiDEr: %.3f" % sc_cid)
+        self.me = sc_me
+        if test:
+            self.score['BLEU4'] = sc_bl
+            self.score['ROUGE'] = sc_rg
+            self.score['METEOR'] = sc_me
+            self.score['CiDEr'] = sc_cid
+            self.ans = results
 
-
-        # image_url = 'https://tensorflow.org/images/surf.jpg'
-        # image_extension = image_url[-4:]
-        # image_path = tf.keras.utils.get_file('image'+image_extension,
-        #                                     origin=image_url)
-
-        # result = self.captiongenerator.evaluate(image_path)
-        # print ('Prediction Caption:', ' '.join(result))
-        # # 画像を開く
-        # Image.open(image_path)
     
     # 入力：正解の辞書，生成文の辞書
     # 出力：各スコアの合計及び各スコア
@@ -115,10 +152,11 @@ class ShowAttendandTell():
         bleu_end = time.time()
 
         ####METEOR###
-        # pycoco_meteor = Meteor()
-        # sc_me, _ = pycoco_meteor.compute_score(
-        #     references, candidates)
-        # meteor_end = time.time()
+        pycoco_meteor = Meteor()
+        sc_me, _ = pycoco_meteor.compute_score(
+            references, candidates)
+        meteor_end = time.time()
+        del pycoco_meteor
 
         ####ROUGE###
         pycoco_rouge = Rouge()
@@ -136,7 +174,7 @@ class ShowAttendandTell():
         #     "Metric times: bleu[{0:.2f}] meteor[{1:.2f}] rouge[{2:.2f}] cider[{3:.2f}]".format(
         #         bleu_end-metric_start, meteor_end-bleu_end, rouge_end-meteor_end, cider_end-rouge_end))
 
-        sc_me = 1.0
+        # sc_me = 1.0
 
         total_score = sc_bl[0] + sc_bl[1] + \
             sc_bl[2] + sc_bl[3] + sc_rg + sc_me + sc_cid
@@ -149,10 +187,7 @@ class ShowAttendandTell():
         new_sentence_list = list()
         # print(sentence_list)
         for s in sentence_list:
-            s_new = s.replace(".", "").replace(" ##", "").replace("?", "").replace("!", "").replace("<", "").replace(">", "").lower()
-            # if not self.dense_captioning:
-            #     s_new = s_new.replace("< robot >", "<robot>")
-            # print(s_new)
+            s_new = s.replace(".", "").replace(" ##", "").replace("?", "").replace("!", "").replace("<", "").replace(">", "").replace("\n", "").lower()
             new_sentence_list.append(s_new)
         return new_sentence_list
 
@@ -163,4 +198,4 @@ if __name__ == '__main__':
             tf.config.experimental.set_memory_growth(device, True)
     model = ShowAttendandTell()
     model.train()
-    model.test()
+    # model.test(model.captiongenerator.img_name_test, model.captiongenerator.cap_test)